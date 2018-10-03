f7a20d0811f1d6e21a1d7b5f1a27002ef78531a0
Catherine Lee <catherine598616@gmail.com>
2018-10-03 14:39:39 -0700

    Added to board test

--- src/test/java/org/jpacman/test/framework/model/BoardTest.java
+++ src/test/java/org/jpacman/test/framework/model/BoardTest.java
@@ -21,40 +21,27 @@ import org.junit.Test;
 public class BoardTest {
 	
 	private Board board;
-	private final Sprite adam = new Sprite() { };
+	private Sprite sprite;
 	
 	/**
 	 * Create a Board, and give it initial height and width.
 	 */
 	@Before public void setUp() {
 		//TODO: Do we need this ?
+		sprite = new Sprite() {};
 		board = new Board(10, 10);
 	}
 
+	
+	//////////////Board Construction Tests//////////////////
 	
 	/**
 	 * Test the Board constructor.
 	 */
 	@Test
-	public void testInvalidInitialization() {
+	public void testInitInvalidWidth() {
 		try {
-			Board b = new Board(-1, 5);
-			fail("Board should not be successfully created.");
-		} catch (AssertionError e) {
-			String message = "PRE1: width should be >= 0 but is -1";
-			assertEquals(message, e.getMessage());
-		}
-		
-		try {
-			Board b = new Board(100, -5);
-			fail("Board should not be successfully created.");
-		} catch (AssertionError e) {
-			String message = "PRE: current should be < max but is -5";
-			assertEquals(message, e.getMessage());
-		}
-		
-		try {
-			Board b = new Board(-1, -500);
+			new Board(-1, 5);
 			fail("Board should not be successfully created.");
 		} catch (AssertionError e) {
 			String message = "PRE1: width should be >= 0 but is -1";
@@ -62,6 +49,28 @@ public class BoardTest {
 		}
 	}
 	
+	@Test
+	public void testInitInvalidHeight() {
+		try {
+			new Board(100, -5);
+			fail("Board should not be successfully created.");
+		} catch (AssertionError e) {
+			String message = "PRE2: height should be >= 0 but is -5";
+			assertEquals(message, e.getMessage());
+		}
+	}
+	
+	
+	@Test
+	public void testInitInvalidWidthHeight() {
+		try {
+			new Board(-1, -500);
+			fail("Board should not be successfully created.");
+		} catch (AssertionError e) {
+			String message = "PRE1: width should be >= 0 but is -1";
+			assertEquals(message, e.getMessage());
+		}
+	}
 	
 	
 	/**
@@ -86,88 +95,141 @@ public class BoardTest {
 	}
 	
 	
+	//////////////Test board and sprite interactions//////////////////
+	@Test
+	public void testPutOffBoard() {
+		try {
+			board.put(sprite, 10, 10);
+			fail("Sprite should not be succesfully put");
+		} catch (AssertionError e) {
+			String message = "PRE1: (10, 10)not on board of size 10 * 10";
+			assertEquals(message, e.getMessage());
+		}
+		
+		//Confirm that the sprite was not put on the board.
+		assertEquals(sprite.getTile(), null);
+	}
+	
+	@Test
+	public void testPutNullSprite() {
+		
+		//test null sprite 
+		try {
+			board.put(null, 5, 5);
+			fail("Sprite should not be successfully put.");
+		} catch (AssertionError e) {
+			String message = "PRE2: Sprite not null";
+			assertEquals(message, e.getMessage());
+		}
+	}
+	
 	/**
 	 * Test the put method that put a sprite at a given position.
 	 */
 	@Test
-	public void testInvalidPut() {
-		//test off the board 
-		try {
-			board.put(adam, 11, 10);
-			fail("Sprite should not be succesfully put");
-		} catch (AssertionError e) {
-			String message = "PRE1: ";
-			assertEquals(message, e.getMessage());
-		}
-		//test null sprite 
-		try {
-			board.put(null, 11, 10);
-			assert(false);
-		} catch (AssertionError e) {
-			
-		}
+	public void testPutOccupiedSprite() {
+		Tile tile = new Tile(1,1);
+
 		//test occupied sprite
 		try {
-			Sprite ben = new Sprite() { };
-			board.put(null, 11, 10);
-		
-		} catch (AssertionError e) {
+			sprite.occupy(tile);
+			board.put(sprite, 5, 5);
+			fail("Sprite should not be successfully put.");
 			
+		} catch (AssertionError e) {
+			String message = "PRE3: Sprite should not occupy[1,1]";
+			assertEquals(message, e.getMessage());
 		}
 		
+		assertEquals(board.spriteAt(5, 5), null);
+	}
+	
+	@Test
+	public void testPutSprite() {
+		board.put(sprite, 5, 5);
+		assertEquals(board.spriteAt(5, 5), sprite);
+	}
+	
+	
+	@Test
+	public void testPutSpriteType() {
+		board.put(sprite, 5, 5);
+		assertEquals(board.spriteTypeAt(5, 5), sprite.getSpriteType());
+	}
+	
+	
+	@Test
+	public void testInvalidSpriteAtType() {
+		try {
+			board.spriteTypeAt(10, 5);
+			fail("Invalid position should fail");
 		
-		board.put(adam, 2, 3);
-		assertTrue(board.tileAt(2, 3).containsSprite(adam));
+		} catch (AssertionError e) {
+			String message = "PRE: (10, 5)not on board of size 10 * 10";
+			assertEquals(message, e.getMessage());
+		}
+
 	}
 	
 	
-	/**
-	 * Test spriteAt
-	 */
 	@Test
-	public void testSpriteAt() {
-		//TODO: Implement me.
-		board.put(adam, 2, 3);
-		assertEquals(adam, board.spriteAt(2, 3));
-	}
-	
-	
-	/**
-	 * Test spriteTypeAt
-	 */
-	@Test
-	public void testSpriteTypeAt() {
-	 //TODO: Implement me.
-		//
-		board.put(adam, 2, 3);
-		assertEquals(SpriteType.OTHER, board.spriteTypeAt(2, 3));
-		assertEquals( SpriteType.EMPTY, board.spriteTypeAt(1, 1));
-	}
-	
-	
-	/**
-	 * Test tileAtOffset
-	 */
-	@Test
-	public void testTileAtOffset() {
-	 //TODO: Implement me.
-		Tile tile = new Tile(2,3);
-		Tile result = board.tileAtOffset(tile, 4, 5);
-		assertEquals(result.getX(), 6);
-		assertEquals(result.getY(), 8);
-	}
-	
+	public void invalidSpriteAt() {
+		try {
+			board.spriteAt(10, 10);
+			fail("Invalid position should fail");
 		
-	/**
-	 * Test tileAtDirection
-	 */
-	@Test
-	public void testTileAtDirection() {
-	 //TODO: Implement me.
-		Tile tile = new Tile(2,3);
-		Tile result = board.tileAtDirection(tile, Direction.RIGHT);
-		assertEquals(result.getX(), 3);
-		assertEquals(result.getY(), 3);
+		} catch (AssertionError e) {
+			String message = "PRE: (10, 10)not on board of size 10 * 10";
+			assertEquals(message, e.getMessage());
+		}
 	}
+	
+	//////////////Test board tiles//////////////////
+	
+	@Test
+	public void testTileAt() {
+		Tile boardTile = board.tileAt(5, 6);
+		assertEquals(boardTile.getX(), 5);
+		assertEquals(boardTile.getY(), 6);
+	}
+	
+	@Test
+	public void testInvalidTileAt() {
+		try {
+			board.tileAt(10, 5);
+			fail("Tile should be out of bounds");
 		
+		} catch (AssertionError e) {
+			String message = "PRE: (10, 5)not on board of size 10 * 10";
+			assertEquals(message, e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testNullTileAtOffset() {
+		try {
+			board.tileAtOffset(null, 5, 5);
+			fail("Null tile should not be valid.");
+		
+		} catch (AssertionError e) {
+			String message = "PRE1: start tile should not be null.";
+			assertEquals(message, e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testInvalidBaseTileAtOffset() {
+		try {
+			board.tileAtOffset(new Tile(10, 10), 0, 0);
+			fail("Base tile should be invalid.");
+		
+		} catch (AssertionError e) {
+			String message = "PRE2(10, 10)not on board of size 10 * 10";
+			assertEquals(message, e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testInvalidTileAtOffset() {
+	}
 }
