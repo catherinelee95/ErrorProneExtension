ca7f9e59f2874622b5120d714f110453670e0512
Alexander Julianto <alexander.julianto94@gmail.com>
2018-10-03 00:14:14 -0700

    Minor fixes on BoardTest initial TODO functions

--- src/test/java/org/jpacman/test/framework/model/BoardTest.java
+++ src/test/java/org/jpacman/test/framework/model/BoardTest.java
@@ -3,9 +3,13 @@ package org.jpacman.test.framework.model;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.jpacman.framework.model.Board;
+import org.jpacman.framework.model.Direction;
 import org.jpacman.framework.model.Sprite;
+import org.jpacman.framework.model.Tile;
+import org.jpacman.framework.model.IBoardInspector.SpriteType;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -22,6 +26,7 @@ public class BoardTest {
 	 * Create a Board, and give it initial height and width.
 	 */
 	@Before public void setUp() {
+		//TODO: Do we need this ?
 		board = new Board(10, 10);
 	}
 
@@ -31,6 +36,7 @@ public class BoardTest {
 	 */
 	@Test
 	public void testInitial() {
+		//TODO: Do we need this ?
 		Board b = new Board(10, 20);
 		assertThat(b.getWidth(), equalTo(10));
 		assertThat(b.getHeight(), equalTo(20));
@@ -42,6 +48,7 @@ public class BoardTest {
 	 */
 	@Test
 	public void testPut() {
+		//TODO: Implement me.
 		board.put(adam, 2, 3);
 		assertTrue(board.tileAt(2, 3).containsSprite(adam));
 	}
@@ -52,7 +59,9 @@ public class BoardTest {
 	 */
 	@Test
 	public void testSpriteAt() {
-	 //TODO: Implement me.
+		//TODO: Implement me.
+		board.put(adam, 2, 3);
+		assertEquals(adam, board.spriteAt(2, 3));
 	}
 	
 	
@@ -62,6 +71,10 @@ public class BoardTest {
 	@Test
 	public void testSpriteTypeAt() {
 	 //TODO: Implement me.
+		//
+		board.put(adam, 2, 3);
+		assertEquals(SpriteType.OTHER, board.spriteTypeAt(2, 3));
+		assertEquals( SpriteType.EMPTY, board.spriteTypeAt(1, 1));
 	}
 	
 	
@@ -71,25 +84,23 @@ public class BoardTest {
 	@Test
 	public void testTileAtOffset() {
 	 //TODO: Implement me.
+		Tile tile = new Tile(2,3);
+		Tile result = board.tileAtOffset(tile, 4, 5);
+		assertEquals(result.getX(), 6);
+		assertEquals(result.getY(), 8);
 	}
 	
-	
+		
 	/**
-	 * Test tunnelledCoordinate
+	 * Test tileAtDirection
 	 */
 	@Test
-	public void testTunnelledCoordinate() {
+	public void testTileAtDirection() {
 	 //TODO: Implement me.
+		Tile tile = new Tile(2,3);
+		Tile result = board.tileAtDirection(tile, Direction.RIGHT);
+		assertEquals(result.getX(), 3);
+		assertEquals(result.getY(), 3);
 	}
-	
-	
-	/**
-	 * Test onBoardMessage
-	 */
-	@Test
-	public void tesOnBoardMessage() {
-	 //TODO: Implement me.
-	}
-	
-	
+		
 }
