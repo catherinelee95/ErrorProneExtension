a784d7eb20bd3ca31c633bdcd3556bc316de1e59
Alexander Julianto <alexander.julianto94@gmail.com>
2018-10-04 15:57:35 -0700

    Added additional test cases for FactoryException constructor

--- src/test/java/org/jpacman/test/framework/factory/FactoryExceptionTest.java
+++ src/test/java/org/jpacman/test/framework/factory/FactoryExceptionTest.java
@@ -1,6 +1,9 @@
 package org.jpacman.test.framework.factory;
 
+import static org.junit.Assert.assertEquals;
+
 import org.jpacman.framework.factory.FactoryException;
+
 import org.junit.Test;
 
 /**
@@ -13,18 +16,47 @@ public class FactoryExceptionTest {
 	 * Test that the Factory Exception inherits Exception with a message parameter. 
 	 * @throws FactoryException 
 	 */
-	@Test(expected = Exception.class)
-	public void testFactoryException() throws Exception {
-		throw new FactoryException("Exception Message");
+	@Test
+	public void testFactoryExceptionContructor() throws Exception {
+		String message = "Exception Message";
+		FactoryException exception = new FactoryException(message);
+		assertEquals(message, exception.getMessage());
 	}
 	
+	
 	/**
-	 * Test that the Factory Exception inherits Exception with a 
-	 * message and throwable parameter.
+	 * Test that the Factory Exception with the message 
+	 * constructor inherits Exception throwable behaviour. 
 	 * @throws FactoryException 
 	 */
 	@Test(expected = Exception.class)
+	public void testFactoryExceptionThrow() throws Exception {
+		throw new FactoryException("Exception Message");
+	}
+	
+	
+	/**
+	 * Test that the Factory Exception inherits Exception with  
+	 * message and throwable parameters.
+	 * @throws FactoryException 
+	 */
+	@Test
 	public void testFactoryExceptionWithCause() throws Exception {
+		String message = "Exception Message";
+		Throwable cause = new Throwable();
+		FactoryException exception = new FactoryException(message, cause);
+		assertEquals(message, exception.getMessage());
+		assertEquals(cause, exception.getCause());
+	}
+	
+	
+	/**
+	 * Test that the Factory Exception with the message and throwable 
+	 * parameters inherits Exception throwable behaviour. 
+	 * @throws FactoryException 
+	 */
+	@Test(expected = Exception.class)
+	public void testFactoryExceptionWithCauseThrow() throws Exception {
 		throw new FactoryException("Exception Message", new Throwable());
 	}
 }
